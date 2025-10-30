0bf6cc7df1482d0a0709001a5e9db0491fc3fcbb
Siddharth Rajit <siddrajit@gmail.com>
2025-10-30 19:19:14 -0300

    Added Loan Qualification Checker workshop

---
+++ JavaScript/Functions/Loan-qualification-checker(Workshop2)/script.js
@@ -0,0 +1,20 @@
+const minIncomeForDuplex = 60000;
+const minCreditScoreForDuplex = 700;
+
+const minIncomeForCondo = 45000;
+const minCreditScoreForCondo = 680;
+
+const minIncomeForCar = 30000;
+const minCreditScoreForCar = 650;
+
+function getLoanMessage(annualIncome, creditScore) {
+  if(creditScore >= minCreditScoreForDuplex && annualIncome >= minIncomeForDuplex) {
+    return "You qualify for a duplex, condo, and car loan."
+  } else if (annualIncome >= minIncomeForCondo && creditScore >= minCreditScoreForCondo) {
+    return "You qualify for a condo and car loan."
+  } else if (annualIncome >= minIncomeForCar && creditScore >= minCreditScoreForCar) {
+    return "You qualify for a car loan."
+  } else {
+    return "You don't qualify for any loans."
+  }
+}
