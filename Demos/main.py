32c5dba47e3079304eb9b0b4cea81f245ee08e1b
Siddharth Rajit <siddrajit@gmail.com>
2025-11-07 20:07:14 -0400

    Added a CSS positioning lab and a Javascript objects lab

--- Demos/main.py
+++
@@ -1,7 +0,0 @@
-def favorite_songs(playlist):
-    target_key = "plays"
-    for dict in playlist:
-      values = (dict[target_key])
-      print(max(values))
-    
-favorite_songs([{"title": "Sync or Swim", "plays": 3}, {"title": "Byte Me", "plays": 1}, {"title": "Earbud Blues", "plays": 2} ])   
